2a9b499cb1784b6182710e6c2e65ac42554c9249
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-05-17 16:08:58 +0300

    Edit view and add doGet() method

--- src/main/java/chat/WebSocketChatServlet.java
+++ src/main/java/chat/WebSocketChatServlet.java
@@ -1,10 +1,18 @@
 package chat;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.eclipse.jetty.websocket.servlet.WebSocketServlet;
 import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
 
+import java.io.IOException;
 
+
+
+@SuppressWarnings("RedundantThrows")
 @WebServlet(name = "WebSocketChatServlet", urlPatterns = "/chat")
 public class WebSocketChatServlet extends WebSocketServlet {
     private final static int LOGOUT_TIME = 10 * 60 * 1000;
@@ -18,5 +26,13 @@ public class WebSocketChatServlet extends WebSocketServlet {
     public void configure(WebSocketServletFactory factory) {
         factory.getPolicy().setIdleTimeout(LOGOUT_TIME);
         factory.setCreator((req, resp) -> new ChatWebSocket(chatService));
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.getWriter().println(PageGenerator.instance().getPage("index.html", null));
+        resp.setContentType("text/html;charset=utf-8");
+        resp.setStatus(HttpServletResponse.SC_OK);
     }
 }
